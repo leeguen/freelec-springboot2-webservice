e0dc724dd0d1d43475be237e09374eafa2a3afdd
leeguen <dlrms6172@naver.com>
2022-08-20 02:01:54 +0900

    naver login

--- src/main/java/com/jojoIdu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoIdu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -31,6 +31,9 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -44,6 +47,22 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributesName)
                 .build();
     }
+
+    private static  OAuthAttributes ofNaver(String userNameAttributesName,
+                                            Map<String,Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributesName)
+                .build();
+    }
+
+
+
 
     public User toEntity(){
         return User.builder()
